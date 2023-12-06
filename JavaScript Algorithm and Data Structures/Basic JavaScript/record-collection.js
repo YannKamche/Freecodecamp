6f9fb20e88683a028d8cfaf92da5a8246ac3c011
Yann Kamche <yannkamche@gmail.com>
2023-12-06 10:38:10 +0100

    Modified the record collection task

--- JavaScript Algorithm and Data Structures/Basic JavaScript/record-collection.js
+++ JavaScript Algorithm and Data Structures/Basic JavaScript/record-collection.js
@@ -21,16 +21,27 @@ const recordCollection = {
 
 // Only change code below this line
 function updateRecords(records, id, prop, value) {
+
+  //Assigns the value to the property if it is not empty and property is not track
   if (prop !== "tracks" && value !== "") {
     records[id][prop] = value;
-  } else if (prop === "tracks" && value !== "" && records[id].hasOwnProperty("tracks") === false) {
-    records[id][prop] = [value];
-  } else if (prop === "tracks" && value !== "") {
-    records[id][prop].push(value);
-  } else if (value === "") {
-    delete records[id][prop];
   }
+
+//Checks if the album doesn't have property
+  else if (prop === "tracks" && records[id].hasOwnProperty("tracks") == false) {
+    records[id][prop] = [];
+    records[id][prop].push(value)
+  } 
+
+  //Checks if the album has property
+  else if (prop === "tracks" && value !== "") {
+    records[id][prop].push(value)
+  }
+
+    //Delete a given property if value is empty
+   else if (value === "") {
+    delete records[id][prop]
+  }
+  
   return records;
 }
-
-updateRecords(recordCollection, 5439, 'artist', 'ABBA');
