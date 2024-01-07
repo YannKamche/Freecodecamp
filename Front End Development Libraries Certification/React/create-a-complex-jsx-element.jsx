f5edb0f3fd5e2bc81e86d0e46fc2773cd3c07b2c
Yann Kamche <yannkamche@gmail.com>
2024-01-07 17:43:49 +0100

    Started React, learnt about JSX, adding comments in JSX, writing JavaScript in JSX

---
+++ Front End Development Libraries Certification/React/create-a-complex-jsx-element.jsx
@@ -0,0 +1,14 @@
+const JSX = (
+    <div>
+        <h1>I am learning React on freeCodeCamp
+        </h1>
+        <p>React is an Open Source view library created and maintained by Facebook. It's a great tool to render the User Interface (UI) of modern web applications.
+        </p>
+        <ul>
+            <li>ReactJs</li>
+            <li>NextJx</li>
+            <li>NuxtJs</li>
+        </ul>
+    </div>
+)
+
