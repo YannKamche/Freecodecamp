c5e0ac784d593500a8c7f9ffca47901f9bd109af
Yann Kamche <yannkamche@gmail.com>
2024-01-08 05:56:04 +0100

    Components, stateless functions, composition

---
+++ Front End Development Libraries Certification/React/create-a-stateless-functional-component.jsx
@@ -0,0 +1,8 @@
+const MyComponent = function() {
+  // Change code below this line
+  return (
+    <div>React requires your function name to begin with a capital letter. Stateless functional component</div>
+  )
+
+  // Change code above this line
+}
