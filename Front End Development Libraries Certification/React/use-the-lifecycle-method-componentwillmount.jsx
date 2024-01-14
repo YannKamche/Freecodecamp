a22a29cda0e7816c9caee12265b9f3c9b50fa7c3
Yann Kamche <yannkamche@gmail.com>
2024-01-14 01:58:20 +0100

    Continued Learning React

---
+++ Front End Development Libraries Certification/React/use-the-lifecycle-method-componentwillmount.jsx
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+  console.log("This is logged to the DOM")
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
