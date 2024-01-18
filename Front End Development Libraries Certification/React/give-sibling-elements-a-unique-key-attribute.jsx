52219ee13b04dc2a6ad8b2b40f30a13dcc5fa10b
Yann Kamche <yannkamche@gmail.com>
2024-01-18 17:55:35 +0100

    Completed React on Freecodecamp

---
+++ Front End Development Libraries Certification/React/give-sibling-elements-a-unique-key-attribute.jsx
@@ -0,0 +1,23 @@
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  
+  const renderFrameworks = frontEndFrameworks.map(framework => 
+    <li key={framework}>{framework}</li>
+  ); // Change this line
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
