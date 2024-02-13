c093838dd2e120b2ca97f4402a571c3c40c662e7
Yann Kamche <yannkamche@gmail.com>
2024-02-13 01:04:16 +0100

    Completed Frontend Certification and added Projects

---
+++ Front End Development Libraries Certification/Projects/Markdown Previewer/index.jsx
@@ -0,0 +1,70 @@
+import * as React from "https://cdn.skypack.dev/react@17.0.1";
+import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+
+const defaultContent = `
+![Dwinatech Logo](https://yt3.ggpht.com/ytc/AKedOLRCB8l9dwq0qVxZIb7C7b1G5lvYlFIYzymD1Dkx=s176-c-k-c0x00ffffff-no-rj)
+
+# Hello, 
+## You'r welcom at
+### DwinaTech Channel
+
+
+\`<div>Inline code</div>\`
+
+\`\`\`
+const multipleLineCode = (param) => {
+    if(param) {
+        return param
+    }
+}
+\`\`\`
+
+**Some bold text**
+
+[Visit My Channel](https://www.youtube.com/channel/UCSS0kFyF7KWjE19Rj9PgNQA)
+
+> Block Quot
+
+1. First list item
+2. Second list item
+`
+
+marked.setOptions({
+  breaks: true,
+  highlight: function (code) {
+    return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+  }
+});
+
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+  return `<a target="_blank" href="${href}">${text}</a>`;
+};
+
+const Editor = ({ content, handleTextareaChange }) => 
+  <textarea value={content} onChange={handleTextareaChange} id="editor" />
+
+const Previewer = ({content}) => (
+  <div id="preview" 
+    dangerouslySetInnerHTML={{
+      __html: marked(content, { renderer: renderer })
+    }}
+  />
+);
+
+const App = () => {
+  const [content, setContent] = React.useState(defaultContent)
+  
+  const handleTextareaChange = (event) => {
+    setContent(event.target.value)
+  }
+
+  return (
+    <div class="main">
+      <Editor content={content} handleTextareaChange={handleTextareaChange} />
+      <Previewer content={content} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
---
+++ Front End Development Libraries Certification/Projects/Random Quote Machine/index.jsx
@@ -0,0 +1,52 @@
+import * as React from "https://cdn.skypack.dev/react@17.0.1";
+import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+
+const quoteData = [
+  { text: `“The purpose of our lives is to be happy.”`, author: "Dalai Lama" },
+  { text: `“Life is what happens when you’re busy making other plans.”`, author: "John Lennon" },
+  { text: `“Get busy living or get busy dying.”`, author: "Stephen King" },
+  { text: `“You only live once, but if you do it right, once is enough.”`, author: "Mae West" },
+  { text: ` “Many of life’s failures are people who did not realize how close they were to success when they gave up.”`, author: "Thomas A. Edison" },
+];
+
+const QuoteBox = ({ quote, handleNewQuote }) => (
+  <div id="quote-box">
+    <p id="text">{quote.text}</p>  
+    <h2 id="author">{quote.author}</h2>
+    <div class="actions">
+      <button 
+        id="new-quote" 
+        class="button"
+        onClick={handleNewQuote} 
+        >
+        New Quote
+      </button>
+      <a 
+         href="https://twitter.com/intent/tweet"
+         id="tweet-quote"
+         target="_blank"
+       >
+         Tweet
+      </a>
+    </div>
+  </div>
+);
+
+const getRandomIndex = (max) => 
+  Math.round(Math.random() * ((quoteData.length-1) - 0) + 0);
+
+const App = () => {
+  const [quote, setQuote] = React.useState(quoteData[getRandomIndex()])
+
+  const handleNewQuote = () => {
+    setQuote(quoteData[getRandomIndex()])
+  }
+
+  return (
+    <div class="main">
+      <QuoteBox handleNewQuote={handleNewQuote} quote={quote} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
